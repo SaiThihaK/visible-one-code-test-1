c9638b655efce5460d159b67c89ad427e3714d72
Sai <saithihak2@gmail.com>
2024-08-21 12:28:03 +0630

    menu bar mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
 import HomeContainer from "@/page-container/home";
+import { NextPage } from "next";
 
-export default function Home() {
+// Need to keep this page server side incase if we need to use meta data as the project scale goes high
+const page: NextPage = () => {
   return <HomeContainer />;
-}
+};
+
+export default page;
